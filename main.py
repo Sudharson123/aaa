d06ae83682ca3dee2a0613a234303de74f69aa0b
Sudharson123 <91829320+Sudharson123@users.noreply.github.com>
2021-12-01 11:58:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+r=str(input('Enter the Row number:'))
+c=str(input('Enter the Column number:'))
+
+rows=6
+columns=6
+while r.isdigit()==False or c.isdigit()==False or int(r) not in range(rows) or int(c) not in range(columns):
+        print('enter digits in range')
+        r = str(input('Enter the Row number:'))
+        c = str(input('Enter the Column number:'))
+        rows = 6
+        columns = 6
+        continue
+
+
+
